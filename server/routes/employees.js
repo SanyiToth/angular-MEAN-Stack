c41d5724c61259829c88b8a25f14905ed7a7ee20
Sanyi Tóth <sandor.toth@hotmail.com>
2021-05-24 15:00:45 +0200

    Create New Endpoint in Node.js – MEAN Stack

--- server/routes/employees.js
+++ server/routes/employees.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
+const Employee = require('../models/employee');
 const router = express.Router();
 
+
 router.get('', function (req, res) {
-  res.json({'success': true});
+  Employee.find({}, function (err, foundEmployees) {
+    res.json(foundEmployees);
+  });
+  /*  res.json({'success': true});*/
+});
+
+router.get('/:id', function (req, res) {
+  const employeeId = req.params.id;
+  Employee.findById(employeeId, function (err, foundEmployee) {
+    if (err) {
+      res.status(422).send({errors: [{title: 'Employee Error!', detail: 'Could not find Employee!'}]})
+    }
+    res.json(foundEmployee);
+  });
 });
 
 
