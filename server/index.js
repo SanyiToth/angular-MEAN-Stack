3cf3d77b60fb7db9b0ecae87d361f73f6752c71c
Sanyi Tóth <sandor.toth@hotmail.com>
2021-05-24 14:51:59 +0200

    separate our Route File from index.js file in Node.js Application

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const config = require('./config/dev');
 const Employee = require('./models/employee');
 const FakeDb = require('./fake-db');
+const employeeRoutes = require('./routes/employees');
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
@@ -10,17 +11,19 @@ mongoose.connect(config.DB_URI, {
 }).then(() => {
   console.log('MongoDb connected');
   const fakeDb = new FakeDb();
+  //push data to mongoDb server
   fakeDb.seedDb();
 })
 
 const app = express();
 
+app.use("/api/v1/employees", employeeRoutes);
 
-app.get('/users', function (req, res) {
+/*app.get('/users', function (req, res) {
 
   res.json({"success": true});
 
-})
+})*/
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
