675c47aa736624e0ba0b30875bb279df8e6c8f95
Sanyi Tóth <sandor.toth@hotmail.com>
2021-05-24 12:37:20 +0200

    Create Rest API Endpoint in Node Js | MEAN Stack

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+
+app.get('/users', function(req, res){
+
+  res.json({"success" : true });
+
+})
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
   console.log("Node Js Server is Running!Port:", PORT);
