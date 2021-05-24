347d36f8653b924a3f014580c58b0054e0627707
Sanyi Tóth <sandor.toth@hotmail.com>
2021-05-24 13:35:06 +0200

    Create Mongoose Model in Node.js | Step By Step

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
 const mongoose = require("mongoose");
-
 const config = require('./config/dev');
+const Employee = require('./models/employee');
+
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
